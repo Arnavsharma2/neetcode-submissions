5e69f9a71b9cf22643599a3efab02dda53d1aa05
Arnav Sharma <73969466+Arnavsharma2@users.noreply.github.com>
2026-09-20 17:15:16 -0500

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 1 , 1 -> 10
+        # 100, 111 -> 1011
+        # 111, 111 -> 14 -> 1110
+
+        carry = 0
+        binA = bin(a)[2:]
+        binB = bin(b)[2:]
+        i = 0
+        res = ''
+        for i in range(-1, -min(len(binA), len(binB))-1, -1):
+            if binA[i] == '1' and binB[i] == '1':
+                if carry == 1:
+                    res += '1'
+                else:
+                    carry = 1
+                    res += '0'
+            else:
+                if carry == 1:
+                    res += '01'
+                    carry = 0
+                else:
+                    res += '1'
+        
+        if carry == 1:
+            res += '1'
+        res = res[::-1]
+        return int(res,2)
+
+
