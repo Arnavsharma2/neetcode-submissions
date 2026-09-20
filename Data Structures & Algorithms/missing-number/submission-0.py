a076689f224ca94f7366d909af3008a090373643
Arnav Sharma <73969466+Arnavsharma2@users.noreply.github.com>
2026-09-20 16:19:20 -0500

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        expected = 0
+        for i in range(len(nums)+1):
+            expected += i
+        return expected-sum(nums)
