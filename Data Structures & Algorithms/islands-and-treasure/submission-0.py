34ccd586bbb42dd5d12d2656474796871dd63a32
Arnav Sharma <73969466+Arnavsharma2@users.noreply.github.com>
2026-09-25 00:54:00 -0500

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        self.visited = set()
+        def dfs(i, j):
+            best = float('inf')
+            if grid[i][j] == -1:
+                return float('inf')
+            elif grid[i][j] == 0:
+                return 0
+
+            DIRECTIONS = [[0,-1], [0,1], [1,0], [-1,0]]
+
+            for d in DIRECTIONS:
+                tempI, tempJ = i + d[0], j + d[1]
+                if (tempI, tempJ) not in self.visited:
+                    if 0 <= tempI < len(grid) and 0 <= tempJ < len(grid[0]):
+                        self.visited.add((tempI, tempJ))
+                        best = min(best, 1 + dfs(tempI, tempJ))
+                        self.visited.remove((tempI, tempJ))
+
+            return best
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2147483647:
+                    grid[r][c] = dfs(r, c)
+                 
