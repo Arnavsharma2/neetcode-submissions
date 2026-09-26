65318ff4fd682c9db151b222d355dbe6c08e8139
Arnav Sharma <73969466+Arnavsharma2@users.noreply.github.com>
2026-09-26 00:12:40 -0500

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for frm, to, price in flights:
+            adj[frm].append((to, price))
+        
+        visited = {}
+        
+        def dfs(curr, moves):
+            if moves > k+1:
+                return float('inf')
+            elif curr == dst:
+                return 0
+            elif (curr, moves) in visited:
+                return visited[(curr, moves)]
+            
+            best = float('inf')
+            for loc, cost in adj[curr]:
+                newPrice = dfs(loc, moves + 1) + cost
+                best = min(best, newPrice)
+
+            visited[(curr, moves)] = best
+            return best
+        
+        return dfs(src, 0)
+
